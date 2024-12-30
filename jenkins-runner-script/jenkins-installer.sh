20c04bbd14e793eebf852d1d36a8c2ba3ef8d86a
Martin Dan <mr.danielyan55@gmail.com>
2024-12-30 15:48:01 -0800

    Add terraform-jenkins demo project

---
+++ jenkins-runner-script/jenkins-installer.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+sudo apt-get update
+yes | sudo apt install openjdk-17-jdk-headless
+echo "Waiting for 30 seconds before installing the jenkins package..."
+sleep 30
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+yes | sudo apt-get install jenkins
+sleep 30
+echo "Waiting for 30 seconds before installing the Terraform..."
+sudo apt-get update
+yes | sudo apt-get install gnupg software-properties-common
+wget -O- https://apt.releases.hashicorp.com/gpg | \
+gpg --dearmor | \
+sudo tee /usr/share/keyrings/hashicorp-archive-keyring.gpg > /dev/null
+gpg --no-default-keyring \
+--keyring /usr/share/keyrings/hashicorp-archive-keyring.gpg \
+--fingerprint
+echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
+https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
+sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+yes | sudo apt-get install terraform
+
+# wget https://releases.hashicorp.com/terraform/1.6.5/terraform_1.6.5_linux_386.zip
+# yes | sudo apt-get install unzip
+# unzip 'terraform*.zip'
+# sudo mv terraform /usr/local/bin/
